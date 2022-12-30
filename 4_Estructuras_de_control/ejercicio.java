042366dd5db2336cd122eb91a4ca3682db600b8a
Severo Iglesias <siglesias@ucol.mx>
2022-12-30 01:07:04 -0600

    add exercise topic 4

---
+++ 4_Estructuras_de_control/ejercicio.java
@@ -0,0 +1,12 @@
+public class Ejercicio {
+    public static void main(String[] args) {
+
+
+        String[] texto = new String[]{"Texto 1", "Texto 2", "Texto 3"};
+
+        for(int i = 0; i < texto.length; i++) {
+            System.out.print(texto[i]);
+        }
+
+    }
+}
